28b629adb495c19cdec897d2315c5eb3cea78e8c
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-03-11 13:46:22 +0530

    One bug in the code

--- Game_of_Life/Game3-GameofLife.cpp
+++ Game_of_Life/Game3-GameofLife.cpp
@@ -23,6 +23,8 @@ After all, life is random generated! */
     #include<random> /* For the engine */
     #include<utility> /* Utility for unrelated domains*/
     #include<functional> /* Functional object for standard hash function*/
+    #include<vector> /* Vector functions header */
+    #include<fstream> /* File operations in C++ */
     #include<algorithm> /* algorithm library but are we going to use it? */
 /* ----------------------------------------------------- */
 /* ----------------------------------------------------- */
@@ -36,14 +38,26 @@ After all, life is random generated! */
             /* 1. Game and Control Related */
                 int start_up_game_choice; /* Variable to choose mode of game*/
             /* 2. Character related variables*/
-                std::string main_character_name, father_name, mother_name; /* The name of the character, duh, and parent's name */
-                int main_character_age=0, father_age,mother_age; /* The age of the character and the parent's age */
-                int main_character_intelligence, main_character_looks, main_character_craziness; /* The character's stats */
+                std::string main_character_name; /* The name of the character, duh*/
+                int main_character_age=0; /* The age of the character */
+                int main_character_intelligence, main_character_looks, main_character_health, main_character_mindframe; /* The character's stats */
+                int main_character_gender=0; /* Start binary in life */
+                double main_character_money=0.0; /* Born in rags, got to wait either for a metal or a silver or no spoon */
         public:
             /* List of functions in the public access-specifier */
             /* 1. start_up() - function gives option if character needs to be generated or user-input. 
-               2. start_up_game() - function to generate the random name and stats */
+               2. start_up_game() - function to generate the random name and stats 
+                   Depending functions: 2.1: reading_names(&file_name):  */
             
+            /* Function 2.1 */
+            static std::vector<std::string> reading_names(const std::string &file_name) {
+                std::vector<std::string> names;
+                std::ifstream file(file_name);
+                for(std::string name; std::getline(file_name, name) && !name.empty(); names.push_back(std::move(name)));
+                return names;             
+            }
+            
+            /* Function 2 */
             void start_up_game() {
                 /* This function sets up the character's details and stats */
                 /* using a dummy variable to action the next set of steps called dummy_start */
@@ -51,10 +65,20 @@ After all, life is random generated! */
                 int dummy_start=start_up();
                 if(dummy_start==1) {
                     /* This is the random mode */
+                    std::random_device rd; /* Random mode*/
+                    std::uniform_int_distribution<int> dist(0,3); /* Setting limit for genders - Code 
+                    0 - Neutral (:|) 1 - Male 2 - Female 3 - Alien */
+                    main_character_gender=dist(rd); /* Setting gender*/
+                    switch(main_character_gender) {
+                        case 0: 
+                                break;
+                        case 1: 
 
+                    }
                 }
             }
 
+            /* Function 1 */
             int start_up() {
                 /* This function 1. introduces the player to choose an option regarding the status of his game */
                 std::cout<<"\tGame of Life!"<<std::endl;
